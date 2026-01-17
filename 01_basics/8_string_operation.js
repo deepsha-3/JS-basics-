08c99f2128b6d923aa8c538ef1c61c0d5c67764c
deepsha-3 <itsmedeepsha375@gmail.com>
2026-01-17 18:31:26 +0545

    code file added

--- 01_basics/8_string_operation.js
+++ 01_basics/8_string_operation.js
@@ -1,2 +1,11 @@
 
 // string operations in JavaScript
+
+let string1 = "Hello"
+let string2 = "Coders!"
+
+// concatenation
+let string3 = string1+string2
+
+console.log("Concatenated String:", string3);
+
